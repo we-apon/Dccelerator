463196f9e877b1f11e241459f29d17d84f73a666
Kirill Dubinkin <programming.weapon@gmail.com>
2016-03-22 16:55:13 +0300

    Added ByteArrayComparer class. It's an IEqualityComparer<byte[]>

---
+++ src/Dccelerator.Core/ByteArrayComparer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Dccelerator {
+    public class ByteArrayComparer : IEqualityComparer<byte[]> {
+        public bool Equals(byte[] left, byte[] right) {
+            if (left == null || right == null) {
+                return left == right;
+            }
+
+            return left.SequenceEqual(right);
+        }
+
+
+        public int GetHashCode(byte[] key) {
+            return key?.Sum(b => b) ?? 0;
+        }
+    }
+}
