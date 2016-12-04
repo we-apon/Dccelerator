2a422cefc3595789662fbe896e9757707bc01e2d
Kirill Dubinkin <prrogramming.weapon@gmail.com>
2016-12-04 17:00:16 +0300

    Fixed, I gues, algorithm of getting most applicable entity info attribute, for concrete target repository

--- src/DataAccess/Dccelerator.DataAccess.Common/Infrastructure/EntityAttributeHelper.cs
+++ src/DataAccess/Dccelerator.DataAccess.Common/Infrastructure/EntityAttributeHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
 using Dccelerator.Reflection;
@@ -9,20 +10,32 @@ namespace Dccelerator.DataAccess.Infrastructure {
     static class EntityAttributeHelper {
 
         [CanBeNull]
-        public static EntityAttribute GetConfigurationForRepository(this Type info, Type repositoryType) {
-
-            var attributes = info.GetInfo().GetCustomAttributes<EntityAttribute>().Where(x => x.Repository == null || repositoryType.IsAssignableFrom(x.Repository)).ToList();
+        public static EntityAttribute GetConfigurationForRepository(this Type info, Type targetRepositoryType) {
+            var attributes = info.GetInfo().GetCustomAttributes<EntityAttribute>().Where(x => x.Repository == null || x.Repository.IsAssignableFrom(targetRepositoryType)).ToList();
             if (attributes.Count == 0)
                 return null;
 
+            if (attributes.Count == 1)
+                return attributes.First();
+
             var cachedAttributes = attributes.OfType<GloballyCachedEntityAttribute>().ToList();
-            var cachedAttribute = cachedAttributes.SingleOrDefault(x => repositoryType.IsAssignableFrom(x.Repository)) ?? cachedAttributes.FirstOrDefault();
+            if (cachedAttributes.Count == 1)
+                return cachedAttributes.First();
+
+            var cachedAttribute = cachedAttributes.MostApplicableAttributeFor(targetRepositoryType) ?? cachedAttributes.FirstOrDefault();
             if (cachedAttribute != null)
                 return cachedAttribute;
 
-            return attributes.SingleOrDefault(x => repositoryType.IsAssignableFrom(x.Repository)) ?? attributes.FirstOrDefault();
+            return attributes.MostApplicableAttributeFor(targetRepositoryType) ?? attributes.FirstOrDefault();
         }
 
+
+        [CanBeNull]
+        static TAttribute MostApplicableAttributeFor<TAttribute>(this IEnumerable<TAttribute> attributes, Type targetRepositoryType) where TAttribute : EntityAttribute {
+            return attributes.Where(x => x.Repository?.IsAssignableFrom(targetRepositoryType) == true)
+                .OrderBy(x => x.Repository.GetGenerationNumberTo(targetRepositoryType)) // tries to find most applicable attribute
+                .FirstOrDefault();
+        }
 
 
     }
