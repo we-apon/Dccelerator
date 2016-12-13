578f66ea77b1b41879057c16d104015223a7c93f
Kirill Dubinkin <prrogramming.weapon@gmail.com>
2016-12-13 12:13:53 +0300

    wel.. published

--- NugetPublish.bat
+++ NugetPublish.bat
@@ -1,6 +1,6 @@
 
 set sourcePath=publish
-set version=0.0.12-rc1
+set version=0.0.12-rc2
 
 nuget push %sourcePath%\Dccelerator.Core.%version%.nupkg -ApiKey %1 -Source https://www.nuget.org/api/v2/package
 
