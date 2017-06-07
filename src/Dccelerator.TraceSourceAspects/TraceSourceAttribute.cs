885c3968d0883422033a2d804cd83224674d7fcb
Kirill Dubinkin <programming.weapon@gmail.com>
2017-06-07 04:02:49 +0300

    TraceSourceAttribute: Added missed AttributeTargets.Assembly

--- src/Dccelerator.TraceSourceAspects/TraceSourceAttribute.cs
+++ src/Dccelerator.TraceSourceAspects/TraceSourceAttribute.cs
@@ -3,8 +3,16 @@ using System;
 
 namespace Dccelerator.TraceSourceAttributes {
 
+    /// <summary>
+    /// <para xml:lang="en"></para>
+    /// <para xml:lang="ru">
+    /// PostSharp-атрибут для трассировки выполнения методов. 
+    /// Трассировка выполняется фреймворком System.Diagnostics.TraceSource, встронным в .Net Framework.
+    /// Кстати, для знакомства с System.Diagnostics.TraceSource хорошо подходит документация расширяюшего его фрейворка - <see hrev="https://essentialdiagnostics.codeplex.com/">Essential.Diagnostics</see>.
+    /// </para>
+    /// </summary>
     [Serializable]
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Assembly)]
     public sealed class TraceSourceAttribute : TraceSourceAttributeBase {
 
     }
