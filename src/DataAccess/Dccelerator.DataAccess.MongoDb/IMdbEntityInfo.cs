b0ea6bd21745dafa351b0b2beea4a852df5cb876
memores <pdorovsky@gmail.com>
2018-02-08 14:44:23 +0300

    MongoDb project transfered to correct folder

--- src/DataAccess/Dccelerator.DataAccess.MongoDb/IMdbEntityInfo.cs
+++ src/DataAccess/Dccelerator.DataAccess.MongoDb/IMdbEntityInfo.cs
@@ -4,10 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dccelerator.DataAccess.MongoDb
-{
-    interface IMdbEntityInfo : IEntityInfo
-    {
-        
-    }
+
+namespace Dccelerator.DataAccess.MongoDb {
+    interface IMdbEntityInfo : IEntityInfo { }
 }
