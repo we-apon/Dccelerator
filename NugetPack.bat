12519bcf1b53cfad6998bf5a247c0b2126d0ffa3
memores <pdorovsky@gmail.com>
2018-03-13 17:39:41 +0300

    Version 0.0.17 release

--- NugetPack.bat
+++ NugetPack.bat
@@ -1,16 +1,18 @@
 
 set versionSuffix=--include-symbols --include-source
-set output=publish
-set buildConfiguration=Debug
+
+set curDir=%cd%
+set output=%curdir%\publish
+set buildConfiguration=Release
 
 del /F /Q %output%\*
 
-#dotnet pack src\Dccelerator.Core -c %buildConfiguration% %versionSuffix% -o %output%
-#dotnet pack src\DataAccess\Dccelerator.DataAccess.Common -c %buildConfiguration% %versionSuffix% -o %output%
-#dotnet pack src\DataAccess\Dccelerator.DataAccess.Lazy -c %buildConfiguration% %versionSuffix% -o %output%
-#dotnet pack src\DataAccess\Dccelerator.DataAccess.BerkeleyDb -c %buildConfiguration% %versionSuffix% -o %output%
-#dotnet pack src\DataAccess\Dccelerator.DataAccess.AdoNet -c %buildConfiguration% %versionSuffix% -o %output%
-#dotnet pack src\DataAccess\Dccelerator.DataAccess.Adapters.Oracle -c %buildConfiguration% %versionSuffix% -o %output%
-#dotnet pack src\DataAccess\Dccelerator.DataAccess.Adapters.MsSql -c %buildConfiguration% %versionSuffix% -o %output%
+dotnet pack src\Dccelerator.Core -c %buildConfiguration% %versionSuffix% -o %output%
+dotnet pack src\DataAccess\Dccelerator.DataAccess.Common -c %buildConfiguration% %versionSuffix% -o %output%
+dotnet pack src\DataAccess\Dccelerator.DataAccess.Lazy -c %buildConfiguration% %versionSuffix% -o %output%
+dotnet pack src\DataAccess\Dccelerator.DataAccess.BerkeleyDb -c %buildConfiguration% %versionSuffix% -o %output%
+dotnet pack src\DataAccess\Dccelerator.DataAccess.AdoNet -c %buildConfiguration% %versionSuffix% -o %output%
+dotnet pack src\DataAccess\Dccelerator.DataAccess.Adapters.Oracle -c %buildConfiguration% %versionSuffix% -o %output%
+dotnet pack src\DataAccess\Dccelerator.DataAccess.Adapters.MsSql -c %buildConfiguration% %versionSuffix% -o %output%
 dotnet pack src\Dccelerator.TraceSourceAspects -c %buildConfiguration% %versionSuffix% -o %output%
 
